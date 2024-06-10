bdd342075b9e448984536017ec2a70f050c1c30c
Haekal Kalipaksi <hkalipaksi@proton.me>
2024-06-10 15:32:49 +0700

    add limiter

---
+++ start/limiter.ts
@@ -0,0 +1,17 @@
+/*
+|--------------------------------------------------------------------------
+| Define HTTP limiters
+|--------------------------------------------------------------------------
+|
+| The "limiter.define" method creates an HTTP middleware to apply rate
+| limits on a route or a group of routes. Feel free to define as many
+| throttle middleware as needed.
+|
+*/
+
+import limiter from '@adonisjs/limiter/services/main'
+
+// @ts-ignore
+export const throttle = limiter.define('global', (ctx) => {
+  return limiter.allowRequests(100).every('1 minute')
+})
